ca2378b05fe5fd162d92731bc39efead16a9d646
creator10569 <vanok10569@gmail.com>
2021-08-28 00:41:49 +0300

    telegram-bot V1.0

--- index.js
+++ index.js
@@ -21,41 +21,63 @@ const keyboard = [
         }
     ]
 ];
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
-  
-    // отправляем сообщение
-    bot.sendMessage(chatId, 'Привет, Друг! чего хочешь?', { // прикрутим клаву
-          reply_markup: {
-              inline_keyboard: keyboard
-          }
-      });
-  });
+const start = () => {
+    bot.setMyCommands( [
+        {command: '/start', description : 'Начальное приветствие'},
+        {command: '/info', description: 'Информация о пользователе'},
+        {command: '/functions', description: 'Функции бота'}
+    ]);
+    bot.on('message', (msg) =>{
+        const text = msg.text;
+        const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
+    
+        if(text === '/start'){
+            // отправляем сообщение
+            bot.sendMessage(chatId, 'Добро пожаловать, я личный бот величайшего программиста сэра @ivanidze14');
+            return bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/5d3/60d/5d360d48-7760-3804-9cd9-6c8e5cda6cb1/12.webp');
+        }
+        if(text === '/info'){
+            // отправляем сообщение
+            return bot.sendMessage(chatId, `Вас зовут ${msg.from.first_name}`);
+        }
+        if(text === '/functions'){
+            const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
+              
+            // отправляем сообщение
+            bot.sendMessage(chatId, 'Привет, Друг! чего хочешь?', { // прикрутим клаву
+                 reply_markup: {
+                     inline_keyboard: keyboard
+                }
+            });
+            
+            return bot.on('callback_query', (query) => {
+                const chatId = query.message.chat.id;
+            
+                let img = '';
+            
+                if(query.data === 'memes') {
+                    img = 'meme.jpg';
+                }
+            
+                if(query.data === 'pivasik') {
+                    img = 'piva.png';
+                }
+            
+                if(img) {
+                    bot.sendPhoto(chatId, img, {
+                        reply_markup: {
+                            inline_keyboard: keyboard
+                        }
+                    });
+    
+                } 
+            });
+        } 
+        else {
+            return bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/404/da2/404da2f2-bf31-485c-a11a-9f9774f546e1/1.webp');
+        }
+        
+    });
+}
 
-bot.on('callback_query', (query) => {
-    const chatId = query.message.chat.id;
-
-    let img = '';
-
-    if(query.data === 'memes') {
-        img = 'meme.jpg';
-    }
-
-    if(query.data === 'pivasik') {
-        img = 'piva.png';
-    }
-
-    if(img) {
-        bot.sendPhoto(chatId, img, {
-            reply_markup: {
-                inline_keyboard: keyboard
-            }
-        });
-    } else {
-        bot.sendMessage(chatId, 'Не понял давай ещё раз', {
-            reply_markup: {
-                inline_keyboard: keyboard
-            }
-        });
-    }
-});
+start();
